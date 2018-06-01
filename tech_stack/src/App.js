e2db7dab0002de61ad6629b2847229c3e1530a9c
nwelchr <nwelchr@gmail.com>
2018-05-31 17:24:11 -0700

    Add list items

---
+++ tech_stack/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import LibraryList from './components/LibraryList';
+
+import { Header } from './components/common';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
